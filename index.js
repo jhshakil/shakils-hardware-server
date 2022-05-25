0b41c958f4e1c54a00647c35ef6c380934030117
Shakil <shakil11271@gmail.com>
2022-05-26 02:07:09 +0600

    create profile

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         const productCollection = client.db('shakilsHardware').collection('products');
         const reviewCollection = client.db('shakilsHardware').collection('reviews');
         const orderCollection = client.db('shakilsHardware').collection('orders');
+        const profileCollection = client.db('shakilsHardware').collection('profiles');
 
         app.get('/product', async (req, res) => {
             const query = {};
@@ -45,11 +46,53 @@ async function run() {
             const myOrder = await orderCollection.find(query).toArray();
             res.send(myOrder);
         })
+        app.get('/profile', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const profile = await profileCollection.findOne(query);
+            res.send(profile)
+        })
 
         app.post('/order', async (req, res) => {
             const data = req.body;
             const order = await orderCollection.insertOne(data)
             res.send(order);
+        })
+
+        app.post('/profile', async (req, res) => {
+            const allData = req.body;
+            const profile = await profileCollection.insertOne(allData);
+            res.send(profile);
+        })
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateData = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateData.available
+                }
+            }
+            const result = await productCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+        })
+        app.put('/profile/:email', async (req, res) => {
+            const email = req.params.email;
+            const updateData = req.body;
+            const query = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    location: updateData.location,
+                    education: updateData.education,
+                    linkedin: updateData.linkedin,
+                    number: updateData.number
+                }
+            }
+            const result = await profileCollection.updateOne(query, updateDoc, options);
+            res.send(result)
         })
 
         app.delete('/order/:id', async (req, res) => {
